37c963a2b1ee3cf7b826e82d8f4e385440deddbc
Kevin Gilpin <kgilpin@gmail.com>
2014-01-06 10:14:39 -0500

    use Upstart provider for Ubuntu >= 13.10

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,9 @@
 node.sshd_service.packages.each{|p| package p }
 
+# https://github.com/AndreyChernyh/openssh/commit/ee011fdda086547c876bceff79f63d751d0893b9
+ssh_service_provider = Chef::Provider::Service::Upstart if 'ubuntu' == node['platform'] && Chef::VersionConstraint.new('>= 13.10').include?(node['platform_version'])
+
 service node.sshd_service.service do
   supports :restart => true
+  provider ssh_service_provider
 end
--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -3,8 +3,9 @@ require 'chefspec'
 describe "sshd-service::default" do
   let(:log_level) { :info }
   let(:attributes) { {} }
-  let(:chef_run) { 
-    ChefSpec::Runner.new(cookbook_path: ['..'], log_level: log_level) do |node|
+  let(:chef_spec_options) { {} }
+  let(:chef_run) {
+    ChefSpec::Runner.new(chef_spec_options.merge(cookbook_path: ['..'], log_level: log_level)) do |node|
       attributes.each do |k,v|
         node.override[k] = v
       end
@@ -25,6 +26,23 @@ describe "sshd-service::default" do
         specify { should install_package(pkg) }
       end
       specify { should define_service("ssh") }
+    end
+    describe "ubuntu" do
+      let(:chef_spec_options) {
+        {
+          platform: 'ubuntu',
+          # fauxhai does not have 13.10 yet
+          version: '13.04'
+        }
+      }
+      describe "13.10" do
+        it "uses upstart as the service provider" do
+          Chef::VersionConstraint.any_instance.should_receive(:include?).and_return true
+          
+          should define_service('ssh')
+          subject.find_resource(:service, "ssh").provider.should == Chef::Provider::Service::Upstart
+        end
+      end
     end
   end
 end
